09d361dabde309d63aef3c4d1b8709f64a99451d
Shivansh Dutta <109021679+EssDee1@users.noreply.github.com>
2024-11-10 03:40:40 -0500

    saved again

--- api/housing.py
+++ api/housing.py
@@ -1,15 +1,20 @@
 #import requests
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import requests
+import os
 
 app = Flask(__name__)
 
 # Replace this with your RentCast API key
-RENTCAST_API_KEY = 'RENTCAST'
+load_dotenv()
+RENTCAST_API_KEY = os.getenv("RENTCAST")
+
 
 @app.route('/api/housing', methods=['GET'])
 def get_housing_recommendations():
-    lat = 30#request.args.get('lat')
-    lng = 170#request.args.get('lng')
+    lat = 30.1481678#request.args.get('lat')
+    lng = -97.7936996#request.args.get('lng')
     radius = request.args.get('radius', 5)  # Default radius is 5 miles if not provided
 
     # Validate that latitude and longitude are provided
@@ -17,16 +22,17 @@ def get_housing_recommendations():
         return jsonify({"error": "Latitude and longitude are required"}), 400
 
     # RentCast API endpoint to search properties by geographic area (latitude, longitude, and radius)
-    rentcast_api_url = f'https://api.rentcast.io/v1/listings/rental/long-term?latitude=30&longitude=170&radius=5'
+    rentcast_api_url = f'https://api.rentcast.io/v1/listings/rental/long-term?latitude={lat}&longitude={lng}&radius={radius}&status=Active&limit='
 
+    payload = ""
     headers = {
-        'Accept': 'application/json',
-        'X-Api-Key': 'RENTCAST',
+        'X-Api-Key': RENTCAST_API_KEY,
+        'accept': 'application/json'
     }
 
     try:
         # Fetch data from RentCast API
-        response = requests.get(rentcast_api_url, headers=headers)
+        response = requests.get(rentcast_api_url, headers=headers, data=payload)
         response.raise_for_status()  # Raise an error for bad status codes
         housing_data = response.json()  # Assuming JSON response
         return jsonify(housing_data)
