1b1fa2d05caa20d8d9aa7de0571e815999336e6a
astarryknight <whiteh4tter@gmail.com>
2024-11-10 07:56:36 -0500

    finish server

--- api/index.py
+++ api/index.py
@@ -5,6 +5,7 @@ import google.generativeai as genai
 from flask import Flask, request, jsonify
 from flask import request
 from flask_cors import CORS
+import json
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
@@ -35,6 +36,11 @@ CORS(
  },
 )
 
+def set_default(obj):
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError
+
 @app.route('/')
 def home():
     return "hwllo world"
@@ -56,30 +62,32 @@ def accept_data():
     data = request.get_json()
     print(data["info"], flush=True)
     #parse data
-    db[data["info"]["f_name"]["email"]] = {data["info"]["f_name"]["lname"]}
+    db["whiteh4tter@gmail.com"] = {data["info"]["f_name"]["lname"]}
     #db[data["info"]["f_name"]["email"]]
 
 
-    print(db["a"], flush=True)
+    #print(db["a"], flush=True)
     #send to DB
 
     #send request to gemini
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    input = "MAKE SURE YOUR RESPONSE IS IN THIS FORMAT: Housing: $___,Meals: $___,Cell Service: $___\nMy budget is "+data["info"]["f_name"]["budget"]+". Please recommend the best budget breakdown for a monthly budget with the following categories: Housing, Meals, and cell service. Give one number, not a range."
+    input = "MAKE SURE YOUR RESPONSE IS IN THIS FORMAT: Housing: $___,Meals: $___,Cell Service: $___\nMy budget is "+str(data["info"]["f_name"]["budget"])+". Please recommend the best budget breakdown for a monthly budget with the following categories: Housing, Meals, and cell service. Give one number, not a range."
     response = model.generate_content(input)
     responses = response.text.split(",")
 
     #send request to rent for housing
-    
+
+
     #return data.info.f_name
 
-    db[data["info"]["f_name"]["email"]] = {"budget_breakdown":{"housing":responses[0].split(":")[1], "meals":responses[1].split(":")[1], "SIM":responses[2].split(":")[1]}, "housing":[[0,0], [0,1], [2,1]]} #ADD coordinates for housing
+    db["whiteh4tter@gmail.com"] = {"housing":[[0,0], [0,1], [2,1]]} #ADD coordinates for housing
+    #"budget_breakdown":{"housing":responses[0].split(":")[0], "meals":responses[1].split(":")[0], "SIM":responses[2].split(":")[0]}, 
     return jsonify({"good": "you did it"}), 200
 
 @app.route('/get_data/<email>')
 def get_data(email):
-    return db[email]
+    return json.dumps(db[email], default=set_default)
     #return jsonify({"a",db[a]}), 200
 
 
